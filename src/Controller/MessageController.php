95b582a3ec6ce89fcf0e2a6d6214de44f14bbb36
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-27 10:42:32 +0200

    delete create msg

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,19 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MessageController extends AbstractController
 {
     /**
      * @Route("/message", name="message")
      */
-    public function index(): Response
+    public function index(MessageRepository $msgRepo): Response
     {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
-        ]);
+        $messages = $msgRepo->findAll();
+        return $this->json($messages);
+    }
+
+    /**
+     * @Route("/message/create", name="messageCreate", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $manager, SerializerInterface $serializer): Response
+    {
+        $data = $request->getContent();
+        $message = $serializer->deserialize($data, Message::class, 'json');
+
+        $manager->persist($message);
+        $manager->flush();
+
+        return $this->json($message);
+    }
+
+    /**
+     * @Route("/message/del/{id}", name="messageDel", methods={"DELETE"})
+     */
+    public function delete(Message $message, Request $request, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($message);
+        $manager->flush();
+
+        return $this->json("OK", 200, []);
     }
 }
