3b12b2dd405d218112260bd776e7c747fcf7b640
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-27 15:27:41 +0200

    Verif token

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * @Route("/api")
+ */
 class MessageController extends AbstractController
 {
     /**
